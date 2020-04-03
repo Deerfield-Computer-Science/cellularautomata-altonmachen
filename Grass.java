d31b6981b16b4968ceb78e7c40e175a239b009f6
altonmachen <amachen20@deerfield.edu>
2020-04-03 17:05:34 -0400

    Updated code and text file

---
+++ Grass.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Grass extends Plant {
+	
+	public Grass(Location l, World w) {
+		super(l,w);
+		myLifeSpan = 3;
+	}
+	
+	public void reproduce() {
+		int newX = (int)(Math.random()*20);
+		int newY = (int)(Math.random()*20); 
+		myWorld.getCreatureList().add(new Grass(new Location(newX,newY), myWorld));
+	}
+
+}
