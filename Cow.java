967c95eefc85f7075b8b2f923fb5e15fe7f532f6
altonmachen <amachen20@deerfield.edu>
2020-04-04 13:52:12 -0400

    Updated cow class

---
+++ Cow.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+public class Cow extends LifeForm {
+
+	public Cow(int myLifeSpan, Location myLocation, Color myColor, World myWorld) {
+		super(myLifeSpan, myLocation, myColor, myWorld);
+		myColor = Color.WHITE;
+	}
+
+	@Override
+	public void reproduce() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void eatGrass() {
+		
+	}
+
+}
